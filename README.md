d8380e89bebebb0608819b0ea2aa0303b0e5bf64
Aleksander Gondek <gondekaleksander@hotmail.com>
2021-01-04 15:19:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bazel-k8s-resources-mngmt
+Example / PoC of using bazel rules for managing different k8s deployments / resources
---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ }:
+
+let
+  pkgs = import (
+    fetchTarball { url = https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz;}
+  ) {};
+in
+pkgs.mkShell {
+  name = "bazel-k8s-resources-mngmt-dev-shell";
+
+  buildInputs = with pkgs; [
+    bash
+    busybox
+    bazel
+    bazel-buildtools
+    cacert
+    coreutils-full
+    kubectl
+    kubernetes-helm
+    kustomize
+  ];
+
+  shellHook = ''
+    mkdir -p ./.helm/cache
+    mkdir -p ./.helm/config
+    mkdir -p ./.helm/data
+    mkdir -p ./.helm/repository-cache
+    export HELM_CACHE_HOME=$(pwd)/.helm/cache
+    export HELM_CONFIG_HOME=$(pwd)/.helm/config
+    export HELM_DATA_HOME=$(pwd)/.helm/data
+    export HELM_REPOSITORY_CACHE=$(pwd)/.helm/repository-cache
+  
+    echo 'Bazel k8s resources mngmt dev shell ready - happy hacking!'
+  '';
+}
